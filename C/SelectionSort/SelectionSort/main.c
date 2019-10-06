bbbe0d687a699d0ce6df415cfd1131b7e9901de6
Michaela Adams <mjadams@fastmail.com>
2019-10-05 20:12:23 -0400

    finished selection sort in C

--- C/SelectionSort/SelectionSort/main.c
+++ C/SelectionSort/SelectionSort/main.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 void moveMin(int * pArrayToSort, int indexOfElement, int element)
 {
-	for (int i = indexOfElement; i > 0; i --)
+	for (int j = indexOfElement; j >= 0; --j)
 	{
-		if (element < pArrayToSort[i])
+		// if the element from the outter loop (element) is less than the 
+		// current element, have them swap places. 
+		if (element < pArrayToSort[j])
 		{
-			int temp = pArrayToSort[i]; 
-			pArrayToSort[i] = element; 
-			pArrayToSort[indexOfElement] = temp; 
-			indexOfElement = i;
+			// declare an intermediate temp variable 
+			int temp = pArrayToSort[indexOfElement];
+			pArrayToSort[indexOfElement] = pArrayToSort[j];
+			pArrayToSort[j] = temp;
+			indexOfElement = j;
 		}
 	}
 }
@@ -17,13 +20,22 @@ void selectionSort(int * pArrayToSort, int arrayToSortLength)
 {
 	for (int i = 0; i < arrayToSortLength; i++)
 	{
+		// This functions serves to move the element at i in the array into its proper position 
+		// in the left half of the array. 
 		moveMin(pArrayToSort, i, pArrayToSort[i]);
 	};
 }
 
 void main()
 {
-	int arrayToSort[] = { 1,5,2 };
+	int arrayToSort[] = { 10,5,4,3,1,5,2 };
 	int arrayToSortLength = sizeof(arrayToSort) / sizeof(int);
 	selectionSort(&arrayToSort, arrayToSortLength);
+
+	// code for printing out the sorted array
+	printf("the array now looks like this: ");
+	for (int i = 0; i < arrayToSortLength; i++)
+	{
+		printf("%d,", arrayToSort[i]);
+	}
 }
