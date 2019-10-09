818c3e1b0d5b3e063ba3a172cae747704b93b7cc
Michaela Adams <mjadams@fastmail.com>
2019-10-09 18:07:20 -0400

    figured out the merge part of merge sort in c

--- C/Merge/Merge/main.c
+++ C/Merge/Merge/main.c
@@ -1,40 +1,59 @@
 #include <stdio.h>
 
-int merge(int* lptr, int* rptr, int* resptr, int resultLength)
+int merge(int* lptr, int* rptr, int rightSize, int leftSize, int* resptr, int resultLength)
 {
 	// currently merges two equal length arrays into a resulting array  
-	int leftIndex = 1;
-	int rightIndex = 1;
-	int leftSize = lptr[0]; 
-	int rightSize = rptr[0];
+	int leftIndex = 0;
+	int rightIndex = 0;
 
 	for (int i = 0; i < resultLength; i++)
 	{
-		// what happends here is that leftIndex is incrmented and now points to nothing memory
-		// maybe I can do something smart here, like make the first element of every array it's size. 
-		// And make arrays 1 indexed. 
+		if (leftIndex <  leftSize && rightIndex < rightSize)
+		{
+			if (lptr[leftIndex] < rptr[rightIndex])
+			{
 
-		if (leftIndex < leftSize && (lptr[leftIndex] < rptr[rightIndex]))
-		{
-			resptr[i] = lptr[leftIndex];
-			leftIndex += 1;
+				resptr[i] = lptr[leftIndex];
+				leftIndex += 1;
+			}
+			else 
+			{
+				resptr[i] = rptr[rightIndex];
+				rightIndex += 1;
+			}
 		}
-		else
+		else 
 		{
-			resptr[i] = rptr[rightIndex];
-			rightIndex += 1;
+			if (leftIndex < leftSize)
+			{
+				resptr[i] = lptr[leftIndex];
+				leftIndex += 1;
+			}
+			if (rightIndex < rightSize)
+			{
+				resptr[i] = rptr[rightIndex];
+				rightIndex += 1;
+			}
 		}
 	}
 }
 
+sort()
+{
+
+}
+
 main()
 {
-	int left[] = { 3,3,5 }; // the first element of each array is the array's size 
-	int right[] = { 3,7,8 }; // the first element of each array is the array size
-	int result[4];
-	int resultLength = 4;
+	// example data 
+	int left[] = { 1,10 };  
+	int right[] = { 7,8,10 }; 
+	int leftSize = 2;
+	int rightSize = 3;
+	int result[5];
+	int resultLength = 5;
 
-	merge(&left, &right, &result, resultLength);
+	merge(&left, &right, rightSize, leftSize, &result, resultLength);
 
 	for (int i = 0; i < 4; i++)
 	{
